1a760e713948a829f1a0d64d883424aaf8a3e957
Артем <babenkoartem505@gmail.com>
2024-03-24 00:45:30 +0200

    Виправлення бази даних

--- DAL/DatabaseContext.cs
+++ DAL/DatabaseContext.cs
@@ -36,6 +36,23 @@ public partial class DatabaseContext : DbContext
             throw new Exception("Невдалось підключитись до бази даних.", ex);
         }
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Список ролів користувача.
+        List<Role> roles = new List<Role>()
+        {
+            new Role() { Id = 1, Name = "Адміністратор", Code = "admin" },
+            new Role() { Id = 2, Name = "Викладач", Code = "teacher" },
+            new Role() { Id = 3, Name = "Студент", Code = "student" },
+            new Role() { Id = 4, Name = "Гість", Code = "guest" }
+        };
+
+        // Назви таблиць та їх початкові дані.
+        modelBuilder.Entity<User>().ToTable("User");
+        modelBuilder.Entity<Course>().ToTable("Course");
+        modelBuilder.Entity<Role>().ToTable("Role").HasData(roles);
+    }
 }
 
 /// <summary> Модель підключення до бд, для десереалізації Json файлу. </summary>
